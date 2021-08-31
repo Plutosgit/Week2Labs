014f9d5cdcc0abe83a28e63e6fc438c6f5d5cddb
Plutosgit <venkataraman_balaji@hotmail.com>
2021-08-30 20:38:07 -0700

    Add project files.

---
+++ Lab1/Program.cs
@@ -0,0 +1,74 @@
+﻿
+/// Week No. 2		Exercise No. Lab1
+/// Solution Name: Week2Labs
+/// @author: Balaji Venkataraman
+/// Date:  August 30, 2021
+///
+/// Problem Statement: Ask the user to enter three numbers, calculate the sum and product of
+/// these 3 numbers; divide the product by the sum, and display results of division to the screen
+/// 
+/// 
+/// Overall Plan:
+/// 1) Print an initial welcoming message to the screen
+/// 2) Prompt the user for 3 integers
+/// 3) Calculate the sum and product of the integers, and divide the product by the sum
+/// 4) Print the division of the product to sum to the screen
+/// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class MyFirstProgram
+    {
+        static void Main(string[] args)
+        {
+            // print a message to the screen
+            Console.WriteLine("Week 2; Lab 1");
+            Console.WriteLine("Hello out there.");
+            Console.WriteLine("I will show the importance of type casting datatypes for you.");
+            Console.WriteLine("Enter a number, press [Enter], then repeat this twice more :");
+
+            // declare two integer variables
+            int n1, n2, n3, sum, prod, div;
+            double div1, div2,div3;
+
+            string userInput;
+            userInput = Console.ReadLine();
+            n1 = Int32.Parse(userInput);
+            n2 = Int32.Parse(Console.ReadLine());
+            n3 = Int32.Parse(Console.ReadLine());
+
+            // calculate the sum of these two numbers
+            sum  = n1 + n2 + n3;
+            prod = n1 * n2 * n3;
+            div  = prod / sum;
+            div1 = (double)(prod / sum);
+            div2 = prod / (double)sum;
+            div3 = (double)prod / sum;
+
+            // print a message along with the sum of the two numbers
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Output");
+            Console.WriteLine("======");
+            Console.WriteLine("Your 3 numbers are: " + n1 + ", " + n2 + ", and " + n3);
+
+            Console.WriteLine("The sum of those three numbers       = " + sum);
+            Console.WriteLine("The product of those 3 numbers       = " + prod);
+            Console.WriteLine("");
+            Console.WriteLine("The division of the prod/sum         = " + div);
+            Console.WriteLine("The division of the (dbl)(prod/sum)  = " + div1);
+            Console.WriteLine("The division of the prod/(dbl)sum    = " + div2);
+            Console.WriteLine("The division of the (dbl)prod/sum    = " + div3);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,71 @@
+﻿
+/// Week No. 2		Exercise No. Lab2
+/// Solution Name: Week2Labs
+/// @author: Balaji Venkataraman
+/// Date:  August 30, 2021
+///
+/// Problem Statement: Ask the user to enter a number, calculate the number of candies and gumballs
+/// redeemable by the player. 
+/// Rule #1) Redemption: 10 coupons/candy, 3 coupons/gumball #2) Candies preferred
+/// 
+/// Overall Plan:
+/// 1) Print an initial welcoming message to the screen
+/// 2) Prompt the user for an integer representing # of coupons won
+/// 3) Calculate the max number of candies & gumballs redeemable
+/// 4) Print the results to the screen
+/// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab2
+{
+
+    class MyFirstProgram
+    {
+        // set constants based on the redemption criteria (10 coupons for each candy & 3 coupons for each gumball)
+        public const int NUM_COUPONS_FOR_A_CANDY   = 10;
+        public const int NUM_COUPONS_FOR_A_GUMBALL = 3;
+
+        static void Main(string[] args)
+        {
+            // declare variables
+            int nNumCouponsWon;
+            int nNumCandies, nNumGumballs;
+            int nRemainingCouponsAfterCandies, nRemainingCouponsAfterGumballs;
+
+            // print a message to the screen
+            Console.WriteLine("Week 2; Lab 2");
+            Console.WriteLine("Hello there. I will calculate the number of candies and");
+            Console.WriteLine("gumballs redeemable for a given number of coupons won!");
+            Console.WriteLine("");
+            Console.Write("Enter the number of coupons won: ");
+            
+            // read the Number of coupons the video game player has won at the arcade
+            string userInput;
+            userInput = Console.ReadLine();
+            nNumCouponsWon = Int32.Parse(userInput);
+
+            // calculate the number of candies & gumballs based on the redemption criteria
+            nNumCandies = nNumCouponsWon / NUM_COUPONS_FOR_A_CANDY;
+            nRemainingCouponsAfterCandies = nNumCouponsWon % NUM_COUPONS_FOR_A_CANDY;
+            nNumGumballs = nRemainingCouponsAfterCandies / NUM_COUPONS_FOR_A_GUMBALL;
+            nRemainingCouponsAfterGumballs = nRemainingCouponsAfterCandies % NUM_COUPONS_FOR_A_GUMBALL;
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Here are the maximum number of rewards redeemable..");
+            Console.WriteLine("===================================================");
+            // print output based on calculations 
+            Console.WriteLine("Number of Candies the player can redeem with " + nNumCouponsWon + " coupons           = " + nNumCandies);
+            Console.WriteLine("Number of Gumballs the player can redeem with " + nNumCouponsWon + " coupons          = " + nNumGumballs);
+            Console.WriteLine("Number of remaining coupons after fully redeeming with " + nNumCouponsWon + " coupons = " + nRemainingCouponsAfterGumballs);
+            Console.WriteLine("");
+
+        }
+    }
+}
